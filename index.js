205d4153e96188d18f19719f722400ae81b13154
Paebo <petibors2@gmail.com>
2021-02-19 00:26:04 +0100

    Init socket.io handshake

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 const PORT = process.env.PORT || 5000;

70e0c148d5ee938e545f174067c18f3b6d7827f1
Paebo <petibors2@gmail.com>
2021-02-19 04:07:04 +0100

    add logger middleware base

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const io = require("socket.io")(http, {
   },
 });
 const path = require("path");
+const logger = require("./middleware/logger");
 require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(logger);
 
 //For React
 /*
